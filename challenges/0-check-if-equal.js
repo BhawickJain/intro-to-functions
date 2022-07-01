7802ab1049df058df4ecf137919a6fb7c9d432f2
Bhawick Jain <bhawick@gmail.com>
2022-07-01 14:55:01 +0100

    add running tests

--- challenges/0-check-if-equal.js
+++ challenges/0-check-if-equal.js
@@ -40,8 +40,8 @@ isEqual(4, -4) should be False
 isEqual(0, 1) should be False
 */
 
-console.log('isEqual(4, 4)', 'should be', true)
-console.log('isEqual(0, 0)', 'should be', true)
-console.log('isEqual(-4, -4)', 'should be', true)
-console.log('isEqual(4, -4)', 'should be', false)
-console.log('isEqual(0, 1)', 'should be', false)
+console.log('isEqual(4, 4): ', isEqual(4, 4), 'should be', true)
+console.log('isEqual(0, 0): ', isEqual(0, 0), 'should be', true)
+console.log('isEqual(-4, -4): ', isEqual(-4, -4), 'should be', true)
+console.log('isEqual(4, -4): ', isEqual(4, -4), 'should be', false)
+console.log('isEqual(0, 1): ', isEqual(0, 1), 'should be', false)
